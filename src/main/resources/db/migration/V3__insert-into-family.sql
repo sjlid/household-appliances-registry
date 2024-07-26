16b62e915ca0e3c037d5cbfa998c129dc27ea394
Ivan <senjohn.0000@gmail.com>
2024-07-26 20:53:47 +0300

    wip flyway

---
+++ src/main/resources/db/migration/V3__insert-into-family.sql
@@ -0,0 +1,14 @@
+INSERT INTO product_family (
+    id, company_manufacturer, country_manufacturer, family_name,
+    buy_online, installment, product_type, models_available
+)
+VALUES (1, 'Samsung', 'Korea', 'Ultra HD', true, true, 'TV', 0);
+VALUES (2, 'LG', 'Korea', 'Mega Sos', true, false, 'CLEANER', 0);
+VALUES (3, 'MSI', 'China', 'Defender', false, true, 'PC', 0);
+VALUES (4, 'Yamaha', 'Japan', 'Extra Cool', false, true, 'FRIDGE', 0);
+VALUES (5, 'PePyaKa', 'Russia', 'Pe10', true, true, 'SMARTPHONE', 0);
+VALUES (6, 'Gorizont', 'China', 'SimpleHD', true, true, 'TV', 0);
+VALUES (7, 'Samsung', 'Korea', 'AirDust', true, false, 'CLEANER', 0);
+VALUES (8, 'Lenovo', 'China', 'Thunderbolt', false, true, 'PC', 0);
+VALUES (9, 'Electrolux', 'Sweden', 'GigaCool', false, true, 'FRIDGE', 0);
+VALUES (10, 'Nokia', 'Finland', '3310', true, true, 'SMARTPHONE', 0);
