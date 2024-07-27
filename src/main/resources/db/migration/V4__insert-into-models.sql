abe3060fdde1eb48c5bb5348eb4908f09427e2bc
Ivan <senjohn.0000@gmail.com>
2024-07-27 16:47:18 +0300

    wip flyway

---
+++ src/main/resources/db/migration/V4__insert-into-models.sql
@@ -0,0 +1,26 @@
+INSERT INTO product_family (
+    id, product_type, model_available, model_color, model_name, model_price,
+    model_serial, model_size, model_volume, model_modes,
+    model_compressor, model_doors, model_category, model_processor, model_camera,
+    model_memory, model_technology, family_id
+)
+VALUES (1, 'TV', true, 'red', 'Screen', ),
+    (2, 'TV', false, 'green', 'View X', ),
+    (3, 'TV', true, 'blue', 'View XXX', ),
+    (4, 'TV', false, 'white', 'Window', ),
+    (5, 'CLEANER', true, 'red', 'Flow', ),
+    (6, 'CLEANER', false, 'green', 'Air Flow', ),
+    (7, 'CLEANER', true, 'blue', 'Windy', ),
+    (8, 'CLEANER', false, 'white', 'Windycool', ),
+    (9, 'PC', true, 'red', 'Max', ),
+    (10, 'PC', false, 'green', '' , ),
+    (11, 'PC', true, 'blue', '', ),
+    (12, 'PC', false, 'white', '', ),
+    (13, 'FRIDGE', true, 'red', '', ),
+    (14, 'FRIDGE', false, 'green', '', ),
+    (15, 'FRIDGE', true, 'blue', )
+    (16, 'FRIDGE', true, 'red', ),
+    (17, 'SMARTPHONE', false, 'green', ),
+    (18, 'SMARTPHONE', true, 'blue', ),
+    (19, 'SMARTPHONE', false, 'white', ),
+    (20, 'SMARTPHONE', true, 'red', )
